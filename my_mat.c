631b4c4f9f38a161d43034aa4d38f0540ef9b6a7
nerya0001 <nerya0001@gmail.com>
2021-11-22 07:40:50 -0800

    the end

--- my_mat.c
+++ my_mat.c
@@ -8,6 +8,7 @@ int tmpMatrix[10][10];
 
 int shortest(int i, int j);
 
+//input
 int matrixInput() {
   for(int i = 0; i < 10; i++) {
     for(int j = 0; j < 10; j++) {
@@ -15,18 +16,10 @@ int matrixInput() {
     }
   }
 
-  // printf("\n");
-  // for(int i = 0; i < 10; i++) {
-  //   printf("|");
-  //   for(int j = 0; j < 10; j++) {
-  //     printf(" %d ", matrix[i][j]);
-  //   }
-  //   printf("|\n");
-  // }
-  // printf("\n");
   return 0;
 }
 
+//is there a path
 int isTherePath(int i, int j) {
   if (shortest(i, j) != -1) {
     return true;
@@ -34,19 +27,21 @@ int isTherePath(int i, int j) {
   return false;
 }
 
+//shortest path
 int shortest(int i, int j) {
   /*do all the calculations and return the size of the shortest path*/
   if (i == j) {
     return -1;
   }
 
+  //filling tmp matrix
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       tmpMatrix[i][j] = matrix[i][j];
     }
   }
 
-
+  //the algorithm
   for (int k = 0; k < 10; k++) {
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 10; j++) {
@@ -63,15 +58,6 @@ int shortest(int i, int j) {
     tmpMatrix[i][i] = 0;
   }
 
-  // printf("\n");
-  // for(int i = 0; i < 10; i++) {
-  //   printf("|");
-  //   for(int j = 0; j < 10; j++) {
-  //     printf(" %d ", tmpMatrix[i][j]);
-  //   }
-  //   printf("|\n");
-  // }
-  // printf("\n");
   if (tmpMatrix[i][j] == 0) {
     return -1;
   } else {
